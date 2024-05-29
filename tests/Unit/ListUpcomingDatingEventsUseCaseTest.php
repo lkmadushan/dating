ff4bbbde38293995a1599b92ab24861c44b608b4
Kalpa Perera <kalpa.lkm@gmail.com>
2024-05-29 20:07:24 +0530

    Upcoming events

--- tests/Unit/ListUpcomingDatingEventsUseCaseTest.php
+++ tests/Unit/ListUpcomingDatingEventsUseCaseTest.php
@@ -9,6 +9,8 @@ use App\Models\User;
 use App\UseCases\ListUpcomingDatingEventsCommand;
 use App\UseCases\ListUpcomingDatingEventsUseCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use MatanYadaev\EloquentSpatial\Enums\Srid;
+use MatanYadaev\EloquentSpatial\Objects\Point;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -20,16 +22,31 @@ class ListUpcomingDatingEventsUseCaseTest extends TestCase
     public function list_dating_events()
     {
         $viewer = User::factory()->create();
-        $viewer->preferred_distance = 100;
+        $viewer->preferred_distance = 120_000;
         $viewer->preferred_distance_only = true;
-        $events = Event::factory(5)->create();
+        $event1 = Event::factory()->create([
+            'location' => new Point(6.9271, 79.8612, Srid::WGS84->value) // Colombo
+        ]);
+        $event2 = Event::factory()->create([
+            'location' => new Point(7.2906, 80.6337, Srid::WGS84->value) // Kandy
+        ]);
+        $event3 = Event::factory()->create([
+            'location' => new Point(7.0840, 80.0098, Srid::WGS84->value) // Gampaha
+        ]);
+        $event4 = Event::factory()->create([
+            'location' => new Point(8.5874, 81.2152, Srid::WGS84->value) // Trincomalee
+        ]);
 
         $usecase = new ListUpcomingDatingEventsUseCase;
         $command = new ListUpcomingDatingEventsCommand;
-        $command->location = $events->first()->location;
-
+        $command->location = new Point(6.9271, 79.8612, Srid::WGS84->value); // Colombo
         $events = $usecase->execute($viewer, $command);
 
-//        dd($events->toQuery()->withDistance('location', $command->location)->get()->toArray());
+        $this->assertSame([
+            $event1->getKey(),
+            $event3->getKey(),
+            $event2->getKey()
+        ], $events->modelKeys());
+        $this->assertFalse($events->contains($event4));
     }
 }
